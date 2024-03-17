c25182e6b327be6499c28ec71c31ca6d2d3fb4f7
akiyama-g-2 <ryo.akiyama112345@gmail.com>
2024-03-17 12:02:47 +0900

    modify .saturday? in calendar.rb

--- calendar.rb
+++ calendar.rb
@@ -6,18 +6,18 @@ require 'date'
 options = {}
 opt = OptionParser.new
 
-  opt.on("-m MONTH",Integer) { |month|
-    options[:month] = month
-  }
+opt.on("-m MONTH",Integer) do |month|
+  options[:month] = month
+end
 
-  opt.on("-y YEAR",Integer) { |year|
-    options[:year] = year
-  }
+opt.on("-y YEAR",Integer) do |year|
+  options[:year] = year
+end
 
 opt.parse!(ARGV)
 
-month = options[:month]||Date.today.month
-year = options[:year]||Date.today.year
+month = options[:month] || Date.today.month
+year = options[:year] || Date.today.year
 
 # 最初の日と最後の日を求める
 first_day = Date.new(year, month, 1)
@@ -27,8 +27,8 @@ last_day = Date.new(year, month, -1)
 puts "#{first_day.strftime('%B %Y').rjust(15)}"
 english_day = ["Su","Mo","Tu","We","Th","Fr","Sa"]
 english_day.each do |name|
-		print "#{name.rjust(2)}" 
-		print " "
+  print "#{name.rjust(2)}" 
+  print " "
 end
 
 puts
@@ -38,10 +38,10 @@ puts
 print "   " * first_day.wday
 # 日付を連続で表示、土曜日で改行
 (first_day..last_day).each do |date|
-	day_of_week = date.strftime('%A')
- 	print "#{date.day.to_s.rjust(2)}"
-	print " "
- 	puts if day_of_week == 'Saturday'
+  day_is_saturday =  date.saturday?
+  print "#{date.day.to_s.rjust(2)}"
+  print " "
+  puts if day_is_saturday
 end
 
 puts
