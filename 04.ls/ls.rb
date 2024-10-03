3c7e958d24962dd760f288ae64de57a24d7dc92f
akiyama <ryo.akiyama112345@gmail.com>
2024-10-03 15:55:12 +0900

    add dup method

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,9 +5,10 @@ COL_COUNT = 3
 LIST_WIDTH = 20
 
 def output_in_col_format(files, col_count)
-  fill_to_multiple_with_nil(files, col_count)
-  row_count = files.size / col_count
-  output = files.each_slice(row_count).to_a.transpose
+  arrays_added_nils = files.dup
+  fill_to_multiple_with_nil(files, col_count, arrays_added_nils)
+  row_count = arrays_added_nils.size / col_count
+  output = arrays_added_nils.each_slice(row_count).to_a.transpose
   output.each do |inner_array|
     inner_array.each do |element|
       print element.to_s.ljust(LIST_WIDTH)
@@ -16,11 +17,11 @@ def output_in_col_format(files, col_count)
   end
 end
 
-def fill_to_multiple_with_nil(files, col_count)
+def fill_to_multiple_with_nil(files, col_count, arrays_added_nils)
   surplus = files.size % col_count
   number_of_nils = surplus.zero? ? 0 : col_count - surplus
-  number_of_nils.times { files << nil }
-  files
+  number_of_nils.times { arrays_added_nils << nil }
+  arrays_added_nils
 end
 
 output_in_col_format(files, COL_COUNT)
